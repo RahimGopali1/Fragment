f7d4d2efa015272420cc26a3d762a4b283ead46b
RahimGopali1 <grahim18@tbc.edu.np>
2021-03-17 21:42:27 +0545

    Task 2 completed

--- app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -6,10 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
-import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
+/**
+ * A simple {@link Fragment} subclass that shows a question
+ * with radio buttons for providing feedback. If the user
+ * clicks "Yes" the text header changes to "Article: Like".
+ * If the user clicks "No" the text header changes to "Thanks".
+ */
 public class SimpleFragment extends Fragment {
 
     // The radio button choice has 3 states: 0 = yes, 1 = no,
@@ -24,20 +28,19 @@ public class SimpleFragment extends Fragment {
     /**
      * Creates the view for the fragment.
      *
-     * @param inflater           LayoutInflater
-     * @param container          ViewGroup
-     * @param savedInstanceState Bundle
-     * @return View
+     * @param inflater           LayoutInflater to inflate any views in the fragment
+     * @param container          ViewGroup of parent view to attach fragment
+     * @param savedInstanceState Bundle for previous state
+     * @return rootView
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment.
         final View rootView = inflater.inflate(R.layout.fragment_simple,
                 container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
-        final RatingBar ratingBar =
-                rootView.findViewById(R.id.ratingBar);
 
         // Set the radioGroup onCheckedChanged listener.
         radioGroup.setOnCheckedChangeListener(
@@ -61,21 +64,12 @@ public class SimpleFragment extends Fragment {
                         }
                     }
                 });
-        ratingBar.setOnRatingBarChangeListener
-                (new RatingBar.OnRatingBarChangeListener() {
-                    @Override
-                    public void onRatingChanged(RatingBar ratingBar,
-                                                float rating, boolean fromUser) {
-                        // Get rating and show Toast with rating.
-                        String myRating = (getString(R.string.my_rating) +
-                                String.valueOf(ratingBar.getRating()));
-                        Toast.makeText(getContext(), myRating,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-
 
         // Return the View for the fragment's UI.
         return rootView;
+    }
+
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
     }
 }
